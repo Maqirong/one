746440e6b7085bdcba9839dfe4599a693ff3f655
Maqirong <2644235018@qq.com>
2022-03-15 17:38:24 +0800

    master

---
+++ logdel.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+time=$(date)
+dellog='/data/linkdood/logs/dellog' 
+log='/data/linkdood/im/java/server-messagestorage/server-messagestorage.log'
+del=`du -s $log | awk   '{print $1}'`
+maxsize=$((1024*10))
+if [ $del -gt $maxsize ] 
+then
+    echo '当前时间为：'$time >> $dellog
+    echo '日志大小为：'$del >> $dellog
+    echo > $log
+    echo '日志清理成功' >> $dellog
+else
+    echo '当前时间为：'$time >> $dellog
+    echo '日志正常：' >> $dellog
+    echo '文件大小为：'$del >> $dellog
+fi
